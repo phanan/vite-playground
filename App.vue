a4a1f2b647952c802794a74141ad0fd52d0fe7c7
An Phan <me@phanan.net>
2020-04-30 15:28:09 +0200

    initial commit

---
+++ App.vue
@@ -0,0 +1,39 @@
+<template>
+  <app-header/>
+  <p>Click the button and delete this element in the code.</p>
+  <p>
+    <span>Count is: {{ count }}</span>
+    <button @click="count++">increment</button>
+  </p>
+</template>
+
+<script>
+import { defineAsyncComponent, reactive } from 'vue'
+import AppHeader from './components/Header.vue'
+
+export default {
+  components: {
+    AppHeader
+  },
+
+  setup () {
+    const state = reactive({
+      count: 0
+    })
+
+    return {
+      ...state
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #4fc08d;
+}
+
+h1, p {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
